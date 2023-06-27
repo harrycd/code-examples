be095f6ef352e7dcbcc0304029835b48b11c88b6
Theocharis Alexopoulos <tacontact@gmail.com>
2023-06-27 15:20:18 +0100

    Adds test for math library pow

--- performance_testing/square_number.py
+++ performance_testing/square_number.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-"""A comparison of different ways to calculate a number's square"""
+"""A comparison of different ways to calculate an integer number's square"""
+import math
+
 import numpy as np
 from time import time
+from math import pow
 
 
 def tester(fun):
@@ -15,6 +18,7 @@ def tester(fun):
 
 def main():
     tester(fun=lambda i: i ** 2)
+    tester(fun=lambda i: math.pow(i, 2))
     tester(fun=lambda i: pow(i, 2))
     tester(fun=lambda i: i * i)
     tester(fun=lambda i: np.power(i, 2))
